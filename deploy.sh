4303e3c5894076fac2a1e8e612efc3e8e3355e34
Nuno Cunha <nunorafael_6@hotmail.com>
2021-10-19 00:01:35 +0100

    Add loading to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
+echo '[.....] 0%'
 git pull
+echo '[>....] 20%'
 rm -rf dist node_modules
+echo '[=>...] 40%'
 npm i
+echo '[==>..] 60%'
 npm run build
+echo '[===>.] 80%'
 npm run start:prod &
+echo '[====>] 100% Done'
