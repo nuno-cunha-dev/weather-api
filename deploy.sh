415e23d98045e5b8c2e4175aade7c03dc28e4c66
Nuno Cunha <nunorafael_6@hotmail.com>
2021-10-19 00:12:37 +0100

    kill old service

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
-echo '[.....] 0%'
+echo '[......] 0%'
 git pull
-echo '[>....] 20%'
+echo '[......] 16%'
 rm -rf dist node_modules
-echo '[=>...] 40%'
+echo '[>.....] 32%'
 npm i
-echo '[==>..] 60%'
+echo '[=>....] 64%'
 npm run build
-echo '[===>.] 80%'
+echo '[==>...] 80%'
+kill -9 ps -aux | grep node | awk '{print $2}'
+echo '[===>..] 96%'
 npm run start:prod &
 echo '[====>] 100% Done'
